913690b066999aca6209da91502bd4b3c1f9faa9
Jonatan Männchen <jonatan@maennchen.ch>
2018-12-29 12:49:27 +0100

    Hosting Setup

--- apps/elixir_security_advisory/lib/elixir_security_advisory/application.ex
+++ apps/elixir_security_advisory/lib/elixir_security_advisory/application.ex
@@ -3,17 +3,16 @@ defmodule ElixirSecurityAdvisory.Application do
 
   use Application
 
-  # alias ElixirSecurityAdvisory.Vulnerabilities.Database
+  alias ElixirSecurityAdvisory.Vulnerabilities.Database
 
   def start(_type, _args) do
-    # try do
-    #   # memory: [Node.self() | Node.list()]
-    #   # Database.create!()
-    # catch
-    #   Amnesia.TableExistsError -> :ok
-    # end
+    try do
+      Database.create!(memory: [Node.self() | Node.list()])
+    catch
+      Amnesia.TableExistsError -> :ok
+    end
 
-    # Database.wait(15_000)
+    Database.wait(15_000)
 
     Supervisor.start_link(
       [{Phoenix.PubSub.PG2, name: ElixirSecurityAdvisory.PubSub}],
